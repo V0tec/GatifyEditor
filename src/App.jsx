87c26dc9dca7507d6f8e3f9508e5123a95b88d34
V0tec <vladislav.zhakun100@gmail.com>
2026-02-08 19:55:54 +0200

    Add base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AFDK from "./pages/AFDK";
 import "./App.css";
@@ -6,10 +6,10 @@ import MultiLanguageFlowchart from "./pages/MultiLanguageFlowchart";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/GatifyEditor">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/afdk" element={<AFDK />} />
+        <Route path="/gatify" element={<AFDK />} />
         <Route
           path="/MultiLanguageFlowchart"
           element={<MultiLanguageFlowchart />}
